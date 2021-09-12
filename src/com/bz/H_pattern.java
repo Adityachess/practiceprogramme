0744628ed298e6b7603bf46883ec094cb429e32a
Adityachess <adityadone9398@gmail.com>
2021-09-12 21:12:58 +0530

    Welcome Message

--- src/com/bz/H_pattern.java
+++ src/com/bz/H_pattern.java
@@ -3,18 +3,7 @@ package com.bz;
 public class H_pattern {
 public static void main(String[] args) {
 	
-	for(int i=0; i<=12; i++) {
-		for (int j=0; j<=12; j++) {
-			if(j==0 || j==12 || i==5) 
-				System.out.print("*");
-			else
-				System.out.print(" ");
-			
-		}
-		System.out.println();
-	}
-		
-		
+	System.out.println("Welcome to GIthuB");
 }
 }
 
